ba7d0a1a25687f606b5a07a46d31bf16b262e489
Samuel Seguins <samuelseguins@gmail.com>
2024-01-27 20:45:57 -0300

    arrrumando rotas

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const http = require('http');
+const port = process.env.PORT || 3000;
+
 const app = express();
-const port = 3000 || process.env.PORT;
+const server = http.createServer(app);
 
 app.get('/', (req,res) => {
     res.send('O capitalismo falhou, falha e falhará em todas as sociedades em que ele colocar seus tentaculos que se baseiam na expropriação e na exploração do homem pelo homem');
