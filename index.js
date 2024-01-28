be36485c28e47198671031db280b3be74cb2faf4
Samuel Seguins <samuelseguins@gmail.com>
2024-01-27 21:14:23 -0300

    atualizando resposta

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const http = require('http');
+const cors = require('cors');
+
 const port = process.env.PORT || 3000;
 
 const app = express();
 const server = http.createServer(app);
+
+app.use(cors());
 
 app.get('/', (req,res) => {
     res.send({'mensagem' : 'O capitalismo falhou, falha e falhará em todas as sociedades em que ele colocar seus tentaculos que se baseiam na expropriação e na exploração do homem pelo homem'});
