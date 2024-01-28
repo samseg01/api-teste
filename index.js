f804cbd83bbad45b38f55fca012d20b669a1a886
Samuel Seguins <samuelseguins@gmail.com>
2024-01-27 21:55:12 -0300

    variaveis de log(testes)

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const http = require('http');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 
 const port = process.env.PORT || 3000;
 
@@ -11,6 +12,14 @@ app.use(cors());
 
 app.get('/', (req,res) => {
     res.send({'mensagem' : 'O capitalismo falhou, falha e falhará em todas as sociedades em que ele colocar seus tentaculos que se baseiam na expropriação e na exploração do homem pelo homem'});
+})
+
+app.use(bodyParser.json());
+app.post('/cred', (req, res) =>{
+    const result = ['host: '+req.headers.host, 'protocolo: '+req.protocol, 'ip: '+req.ip, 'ips'+req.ips];
+    console.log(result);
+    console.log(req.body);
+    res.send({'mensagem_back':'dados aceitos'});
 })
 
 app.listen(port, () =>{
